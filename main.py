cc19002ad582875440a0b8584334b97f480d1f1b
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-20 15:32:03 -0700

    Add header, description, image and url for all apps in cols

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -14,3 +15,23 @@ with col2:
     st.info(content)
 
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(row['url'])
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(row['url'])
