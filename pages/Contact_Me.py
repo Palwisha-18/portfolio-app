d53c47eb83da8be86aba9366e12e3da46461e2c9
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-20 15:53:20 -0700

    Created Contact Me form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header('Contact Me')
+
+with st.form(key='email_form'):
+    user_email = st.text_input("Your email address")
+    user_message = st.text_area("Your message")
+    button = st.form_submit_button()
+    if button:
+        print('Pressed')
