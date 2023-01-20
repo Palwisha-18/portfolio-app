bac4a9e320a1efcb92b0c62e33439ef040c1b21b
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-20 15:00:10 -0700

    Create cols, add image and description

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.jpeg')
+
+with col2:
+    st.title('Palwisha Akhtar')
+    content = "Hello 👋, I'm Palwisha!.  I am a Python Developer from Calgary, Canada. " \
+              "I am currently working at Arbisoft as a Senior Software Engineer. "
+    st.info(content)
