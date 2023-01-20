5a7890dea845f3a69900925973d51c140e7b5ed6
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-20 15:13:20 -0700

    Add description on the new col

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ with col1:
 
 with col2:
     st.title('Palwisha Akhtar')
-    content = "Hello 👋, I'm Palwisha!.  I am a Python Developer from Calgary, Canada. " \
-              "I am currently working at Arbisoft as a Senior Software Engineer. "
+    content = """ Hello 👋, I'm Palwisha!.  I am a Python Developer from Calgary, Canada.
+     I am currently working at Arbisoft as a Senior Software Engineer."""
+
     st.info(content)
+
+st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
