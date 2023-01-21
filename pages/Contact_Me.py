8960fcbebc7a6596fdf9da53623ff5d69b143abe
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-20 19:51:29 -0700

    Added email format and secured username and password

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
+from send_email import send_email
+
 st.header('Contact Me')
 
 with st.form(key='email_form'):
     user_email = st.text_input("Your email address")
     user_message = st.text_area("Your message")
     button = st.form_submit_button()
+    email_content = f"""\
+Subject: Email from {user_email}
+    
+
+From: {user_email}
+{user_message}
+"""
     if button:
-        print('Pressed')
+        send_email(email_content)
+        st.info('Message Sent!')
